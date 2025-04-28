b7016e759e8e5186f8312c835a706e2a4749ab59
Breno <brenomelogabriel2019@gmail.com>
2025-04-28 20:41:13 -0300

    feat: integrate better-auth for authentication and set up database schemas
    - Added better-auth package for improved authentication handling.
    - Created database schemas for user, session, account, and verification.
    - Established a connection to the PostgreSQL database using drizzle-orm.
    - Configured dotenv for environment variable management.
    - Implemented auth client and auth logic using better-auth with drizzle adapter.

---
+++ lib/auth-client.ts
@@ -0,0 +1,12 @@
+import { createAuthClient } from "better-auth/react";
+import "dotenv/config";
+
+export const authClient = createAuthClient({
+  /** The base URL of the server (optional if you're using the same domain) */
+
+  baseURL: process.env.NEXT_PUBLIC_API_URL!,
+  fetchOptions: {
+    credentials: "include",
+    // <-- isso aqui precisa estar!
+  },
+});
