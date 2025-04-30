08f8c05c15b90b8725939c259b8e71db4e48ab74
Pedro Baptista <pedrolk2012@gmail.com>
2025-04-30 20:45:16 -0300

    refactor: comment out population function calls in populate.ts

--- scripts/populate.ts
+++ scripts/populate.ts
@@ -1,17 +1,11 @@
-import { populateCustomers } from "./customers/populate-customers";
-import { populateGeolocation } from "./geolocation/populate-geolocation";
-import { populateOrder } from "./order/populate-order";
-import { populateProductCategoryNameTranslation } from "./productCategoryNamerTranslation/populate-product-category-name-translation";
-import { populateSeller } from "./seller/populate-seller";
-
 async function main() {
   try {
-    await populateCustomers();
-    await populateGeolocation();
-    await populateOrder();
-    await populateOrder();
-    await populateSeller();
-    await populateProductCategoryNameTranslation();
+    // await populateCustomers();
+    // await populateGeolocation();
+    // await populateOrder();
+    // await populateOrder();
+    // await populateSeller();
+    // await populateProductCategoryNameTranslation();
   } catch (error) {
     console.error("Error populating database:", error);
   }
