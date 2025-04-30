154cb1752c65f9de08708727cc4e9cc400b30102
Pedro Baptista <pedrolk2012@gmail.com>
2025-04-30 17:27:48 -0300

    feat: extend database schema with new tables for order review, product, seller, and product category translations

--- db/schemas/schema.ts
+++ db/schemas/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { numeric, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { user } from "./auth-schema";
 
 export const chat = pgTable("chat", {
@@ -20,6 +20,65 @@ export const message = pgTable("message", {
   sendAt: timestamp("send_at").notNull(),
 });
 
+export const customer = pgTable("customer", {
+  id: text("id").primaryKey(),
+  customerUniqueId: text("customer_unique_id").notNull(),
+  customerZipCodePrefix: text("customer_zip_code_prefix").notNull(),
+  customerCity: text("customer_city").notNull(),
+  customerState: text("customer_state").notNull(),
+});
+
+export const geolocation = pgTable("geolocation", {
+  geolocationZipCodePrefix: text("geolocation_zip_code_prefix").primaryKey(),
+  geolocationLat: numeric("geolocation_lat").notNull(),
+  geolocationLng: numeric("geolocation_lng").notNull(),
+  geolocationCity: text("geolocation_city").notNull(),
+  geolocationState: text("geolocation_state").notNull(),
+});
+
+export const orderItem = pgTable("order_item", {
+  id: numeric("id").primaryKey(),
+  orderId: text("order_id")
+    .notNull()
+    .references(() => order.id, { onDelete: "cascade" }),
+  productId: text("product_id")
+    .notNull()
+    .references(() => product.id, {
+      onDelete: "cascade",
+    }),
+  sellerId: text("seller_id")
+    .notNull()
+    .references(() => seller.id, {
+      onDelete: "cascade",
+    }),
+  shippingLimitDate: timestamp("shipping_limit_date").notNull(),
+  price: numeric("price").notNull(),
+  freightValue: numeric("freight_value").notNull(),
+});
+
+export const orderPayment = pgTable("order_payments", {
+  id: text("id").primaryKey(),
+  orderId: text("order_id")
+    .notNull()
+    .references(() => order.id, { onDelete: "cascade" }),
+  paymentSequential: text("payment_sequential").notNull(),
+  paymentType: text("payment_type").notNull(),
+  paymentInstallments: numeric("payment_installments").notNull(),
+  paymentValue: numeric("payment_value").notNull(),
+});
+
+export const orderReview = pgTable("order_review", {
+  id: text("id").primaryKey(),
+  orderId: text("order_id")
+    .notNull()
+    .references(() => order.id, { onDelete: "cascade" }),
+  reviewScore: numeric("review_score").notNull(),
+  reviewCommentTitle: text("review_comment_title"),
+  reviewCommentMessage: text("review_comment_message"),
+  reviewCreationDate: timestamp("review_creation_date").notNull(),
+  reviewAnswerTimestamp: timestamp("review_answer_timestamp"),
+});
+
 export const order = pgTable("order", {
   id: text("id").primaryKey(),
   customerId: text("costumer_id")
@@ -38,11 +97,31 @@ export const order = pgTable("order", {
     "order_estimated_delivery_date"
   ).notNull(),
 });
-
-export const customer = pgTable("customer", {
+export const product = pgTable("product", {
   id: text("id").primaryKey(),
-  customerUniqueId: text("customer_unique_id").notNull(),
-  customerZipCodePrefix: text("customer_zip_code_prefix").notNull(),
-  customerCity: text("customer_city").notNull(),
-  customerState: text("customer_state").notNull(),
+  productUniqueId: text("product_unique_id").notNull(),
+  productCategoryName: text("product_category_name").notNull(),
+  productNameLength: numeric("product_name_length").notNull(),
+  productDescriptionLength: numeric("product_description_length").notNull(),
+  productPhotosQty: numeric("product_photos_qty").notNull(),
+  productWeightG: numeric("product_weight_g").notNull(),
+  productLengthCm: numeric("product_length_cm").notNull(),
+  productHeightCm: numeric("product_height_cm").notNull(),
+  productWidthCm: numeric("product_width_cm").notNull(),
 });
+
+export const seller = pgTable("seller", {
+  id: text("id").primaryKey(),
+  sellerUniqueId: text("seller_unique_id").notNull(),
+  sellerZipCodePrefix: text("seller_zip_code_prefix").notNull(),
+  sellerCity: text("seller_city").notNull(),
+  sellerState: text("seller_state").notNull(),
+});
+
+export const productCategoryNameTranslation = pgTable(
+  "product_category_name_translation",
+  {
+    productCategoryName: text("product_category_name").primaryKey(),
+    productCategoryNameEnglish: text("product_category_name_english").notNull(),
+  }
+);
