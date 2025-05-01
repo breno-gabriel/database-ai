cd0942683a88d9d4a4a3ef32491c7fff9fd8ec26
Pedro Baptista <pedrolk2012@gmail.com>
2025-05-01 15:51:55 -0300

    feat: implement chat creation API and update chat landing page with mutation logic

--- app/(app)/chat/page.tsx
+++ app/(app)/chat/page.tsx
@@ -1,11 +1,32 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import axiosClient from "@/lib/axios-client";
+import { useMutation } from "@tanstack/react-query";
 import { PlusCircle } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function ChatLanding() {
+  const router = useRouter();
+
+  const { mutate, isPending, isSuccess } = useMutation({
+    mutationFn: async () => {
+      const response = await axiosClient.post("/api/chat/create");
+      return response.data;
+    },
+    onSuccess(data) {
+      console.log("Chat created successfully:", data);
+      // Redirect to the chat page or perform any other action
+      router.push(`/chat/${data.chatId}`);
+    },
+  });
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-bold">Bem-vindo ao Database AI</h1>
-      <Button className="mt-4 text-lg">
+      <Button
+        className="mt-4 text-lg"
+        disabled={isPending || isSuccess}
+        onClick={() => mutate()}
+      >
         Criar novo Chat <PlusCircle />
       </Button>
     </div>
--- app/api/chat/create/route.ts
+++ app/api/chat/create/route.ts
@@ -1,21 +1,35 @@
+import { chat } from "@/db/schemas";
+import { db } from "@/drizzle";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid";
 
+export async function POST() {
+  try {
+    const session = await auth.api.getSession({
+      headers: await headers(), //some endpoint might require headers
+    });
 
+    if (!session?.user.id) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    const response = await db
+      .insert(chat)
+      .values({
+        id: uuidv4(),
+        userId: session?.user.id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      })
+      .returning();
+    return NextResponse.json({ chatId: response[0].id }, { status: 200 });
+  } catch (error) {
+    console.error("Error in POST request:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
